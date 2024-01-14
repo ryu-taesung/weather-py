65d05dd895dae96f4b27f71595de38010bce6b1b
Ryu, Tae Sung <ryu.taesung@gmail.com>
2024-01-14 14:54:36 -0500

    display before dawn as prev day

--- weather-py.py
+++ weather-py.py
@@ -13,13 +13,21 @@ def get_day_of_week(date_string):
 try:
     with urllib.request.urlopen('https://api.weather.gov/gridpoints/CTP/71,63/forecast') as req:
         data = req.read()
+        # with open('forecast_overnight.json') as f:
+        #     data = f.read()
         api_data = json.loads(data)
         periods = defaultdict(lambda: {"day_of_week": None, 'high_temp': None, 'low_temp': None, 'day_forecast': None, 'night_forecast': None})
     for period in api_data['properties']['periods']:
         forecast = period['detailedForecast']
         match = re.search(r'(?:(?:high near)|(?:low around))\s+(-?\d{1,3})[\.,]', forecast.lower())
         period_date = period['startTime'].split('T')[0]
-        periods[period_date]
+        period_time_start = period['startTime'].split('T')[1]
+        before_dawn = period_time_start.startswith('00')
+        if before_dawn:
+            current_date = dt.datetime.strptime(period_date, '%Y-%m-%d')
+            new_date = current_date - dt.timedelta(days=1)
+            period_date = new_date.strftime('%Y-%m-%d')
+        
         periods[period_date]['day_of_week'] = get_day_of_week(period_date)
 
         if match:
@@ -31,10 +39,10 @@ try:
                 periods[period_date]['low_temp'] = match.group(1)
                 periods[period_date]['night_forecast'] = period['shortForecast']
 
-    print(f"{'Date':<12} {'Day':<4} {'High':<4} {'Low':<4} {'Day Forecast':<40} {'Night Forecast':<40}")
-    print("-" * (104 + 6))
+    print(f"{'Date':<12} {'Day':<4} {'High':<4} {'Low':<4} {'Day Forecast':<49} {'Night Forecast':<49}")
+    print("-" * (122 + 6))
     for k,v in periods.items():
-        print(f"{k:<12} {v['day_of_week']:<4} {v['high_temp'] if v['high_temp'] is not None else 'N/A':<4} {v['low_temp'] if v['low_temp'] is not None else 'N/A':<4} {v['day_forecast'][0:40] if v['day_forecast'] is not None else 'N/A':<40} {v['night_forecast'][0:40] if v['night_forecast'] is not None else 'N/A':<40}")
+        print(f"{k:<12} {v['day_of_week']:<4} {v['high_temp'] if v['high_temp'] is not None else 'N/A':<4} {v['low_temp'] if v['low_temp'] is not None else 'N/A':<4} {v['day_forecast'][0:49] if v['day_forecast'] is not None else 'N/A':<49} {v['night_forecast'][0:49] if v['night_forecast'] is not None else 'N/A':<49}")
 except urllib.error.HTTPError as e:
     # Handle HTTP errors here
     print("Error: weather.gov API currently unavailable.")
